3b67d5580f2e2fef7ee544433fa8e453a885b2c9
Madhushri N <madhushrin6@gmail.com>
2025-10-14 18:31:25 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,121 @@
+export default function Header(){
+  return(
+    <div><div className="dark:bg-white bg-black h-[600px] md:h-[900px] rounded-[80px] items-center flex flex-col">
+<div className="bg-black w-full md:w-[1000px] h-[80px] items-center flex flex-row py-2 pr-3">
+  <img src="https://themehealer.com/php-template/crank-php/assets/images/resources/logo-1.png"></img>
+  <div className=" hidden md:block font-bold text-white mx-[20px] text-lg">Home</div>
+   <div className="hidden md:block font-bold text-white mx-[20px] text-lg">About us</div>
+    <div className="hidden md:block font-bold text-white mx-[20px] text-lg">Pages</div>
+     <div className="hidden md:block font-bold text-white mx-[20px] text-lg">Services</div>
+      <div className="hidden md:block font-bold text-white mx-[20px] text-lg">Shop</div>
+       <div className="hidden md:block font-bold text-white mx-[20px] text-lg">Blog</div>
+        <div className="hidden md:block font-bold text-white mx-[20px] text-lg">Contact</div>
+        <div className="hidden md:block rounded-[20px] px-[20px] dark:text-black bg-white mx-[10px]">F</div>
+        <div className="hidden md:block rounded-[20px] px-[20px] dark:text-black bg-white mx-[10px]">I</div>
+        <div className="hidden md:block rounded-[20px] px-[20px] dark:text-black bg-white mx-[10px]">T</div>
+        
+</div>
+<div className=" items-center flex flex-row px-[10px] w-[350px] md:w-[1000px] text-[30px] md:text-[50px] dark:text-black text-white">
+  <div className=" w-[250px] md:w-[1000px] flex flex-col justify-center">
+  <div className="w-[250px] md:w-[1000px] font-bold mt-[40px]">Where Quality Is A Service<br></br> Meets The<br></br> <span className="text-red-600">Open Road</span></div>
+  <div><p className="text-[15px] md:text-[20px] mr-4">Car service is essential for maintaining the performance and longetivity of your vehicle.From oil changes car service.</p></div>
+  <div className=" flex flex-col items-center justify-center rounded-[100px] h-[50px] md:h-[50px] w-[100px] md:w-[150px] mt-[10px] bg-black border-[1px] border-red-700">
+    <div className="text-[15px] md:text-[25px] mx-[10px] bg-black text-white">Get started</div></div></div>
+  <div className=" flex flex-col h-[100px] w-[100px]"><img className=" mt-[50px] rounded-[20px]"src="https://themehealer.com/php-template/crank-php/assets/images/resources/main-slider-img-2.jpg"></img></div>
+  <div className=" flex flex-row bg-white w-[1000px] h-[100px] absolute bottom-[-40px] rounded-md shadow-white shadow-md border dark:border-black text-white items-center justify-evenly space-x-2.5 ">
+    <div className="font-extrabold text-black text-4xl">600+</div>
+    <div className="font-extrabold text-black text-4xl">2K+</div>
+    <div className="font-extrabold text-black text-4xl">53+</div>
+    <div className="font-extrabold text-black text-4xl">3K+</div>
+    </div>
+  </div> 
+  </div>
+  
+  <div className="flex flex-col items-center justify-center h-[900px] w-full md:w-[full] bg-gray-50 rounded-[50px] shadow-2xl dark:bg-black dark:border-white dark:text-white mt-4 ">
+    <div className="text-red-600 font-bold text-[20px] md:text-[30px] mt-[100px]">Latest Service
+
+    </div>
+    <div className="font-bold text-[20px] md:text-[30px] ">
+      Your trusted Car Provider <br></br>
+        Keeping Your Vehicle
+    </div>
+    <div className="flex flex-col md:flex flex-row space-x-2 mt-[40px]">
+      <div className="flex flex-col items-center w-[100px] md:w-[400px] h-[350px] border-2 border-black dark:border-white  hover:bg-gradient-to-r from-blue-900 to-red-800  ml-[50px] hover:text-white rounded-[30px]">
+      <div className="rounded-full bg-blue-900 h-[80px] w-[80px] mt-[75px] mb-[10px]"></div><div className="font-bold text-[30px] mx-[10px]">Transformation advising<br></br></div>
+      <div className="text-[18px] mx-[25px] mt-2 mb-1">Car service is essential for maintaining the performance and longevity of your vehicle. From oil changes Car service</div>
+   </div>
+   <div className="flex flex-col items-center w-[400px] h-[350px] border-2 border-black dark:border-white  hover:bg-gradient-to-r from-blue-900 to-red-800  ml-[50px] hover:text-white rounded-[30px]">
+      <div className="rounded-full bg-blue-900 h-[80px] w-[80px] mt-[75px] mb-[10px]"></div><div className="font-bold text-[30px] mx-[10px]">Easy drive maintenance<br></br></div>
+      <div className="text-[20px] mx-[25px] mt-2 mb-1">Car service is essential for maintaining the performance and longevity of your vehicle. From oil changes Car service</div>
+   </div>
+   <div className="flex flex-col items-center w-[400px] h-[350px] border-2 border-black dark:border-white  hover:bg-gradient-to-r from-blue-900 to-red-800  ml-[50px] hover:text-white rounded-[30px]">
+      <div className="rounded-full bg-blue-900 h-[80px] w-[80px] mt-[75px] mb-[10px]"></div><div className="font-bold text-[30px] mx-[10px]">Elite Auto Services<br></br></div>
+      <div className="text-[20px] mx-[25px] mt-2 mb-1">Car service is essential for maintaining the performance and longevity of your vehicle. From oil changes Car service</div>
+   </div>
+
+    
+    </div>
+    </div>
+  
+  
+  <div className="bg-white w-full h-[600px] rounded-[40px]  flex flex-row items-center">
+    <div className="flex flex-col pr-4 mt-5 h-[400px] w-[800px] bg-white dark:text-black px-10">
+      <div className="text-red-600 text-[20px] pr-4 flex flex-col ml-8">About Us</div>
+      <div className=" text-[20px] md:text-[40px] text-black pr-4 ml-8">Fast and Reliable Car Care<br></br>Your Car Our Priority</div>
+      <div className="text-[10px] md:text-[20px] text-black pr-4 ml-8  mt-2">Car service is essential for maintaining the performance and longevity of your vehicle. From oil changes Car service</div>
+      <div className="font-bold ml-8 mt-5 text-[10px] md:text-[20px]">Fast and Reliable Car Care Your Car Our Priority</div>
+      <div className="font-bold ml-8 mt-1 text-[10px] md:text-[20px]">Free with pur services care for your car</div>
+      <div className="font-bold ml-8 mt-1 text-[10px] md:text-[20px]">Top notch care for your vehicle</div>
+      <div className="font-bold ml-8 mt-1 text-[10px] md:text-[20px]">Express service for your vehicle drive stress</div>
+      <div className="font-bold ml-8 mt-1 text-[10px] md:text-[20px]">Your trusted car service provider</div>
+      
+      </div>
+      <div className=" flex flex-row">
+        <img className="h-[400px] w-[500px] mt-[50px] ml-[10px] mr-[40px] rounded-[20px]" src="https://themehealer.com/php-template/crank-php/assets/images/resources/main-slider-img-2.jpg"></img>
+      </div>
+      <div className="flex flex-row"> <div className="flex flex-col w-full h-[100px] rounded-[10px] bg-white"></div></div>
+     
+
+  </div>
+  <div className=" bg-black h-[100px] w-[1000px] mx-[300px] rounded-[20px] flex flex-row mt-10 mb-10 dark:bg-white dark:text-black">
+    <div className="flex flex-col justify-center text-white dark:text-black font-bold text-[40px] mx-[50px]">Dropbox</div>
+    <div className="flex flex-col justify-center text-white dark:text-black font-bold text-[40px] mx-[50px]">Spotify</div>
+    <div className="flex flex-col justify-center text-white dark:text-black font-bold text-[40px] mx-[50px]">Coinbase</div>
+    <div className="flex flex-col justify-center text-white dark:text-black font-bold text-[40px] mx-[50px]">Slack</div>
+  </div><div>
+    <div className="hidden md:block mt-[50px] bg-white w-full h-[800px] rounded-[40px] items-center" >
+   <div className=" pr-4 mt-10 h-[400px] w-[800px] bg-white px-10">
+      <div className="text-red-600 text-[20px] pr-4  ml-8">Latest gallery</div>
+      <div className="text-[40px] text-black pr-4 ml-8 font-bold">Quality Service, Every Time<br></br>Drive Safe Stay Secure</div>
+      <div className="text-[20px] text-black pr-4 ml-8 w-[600px] mt-2">Car service is essential for maintaining the performance and longevity of your vehicle. From oil changes Car service</div>
+      <div className="flex flex-row space-x-10 px-[1px] justify-center mt-[40px]">
+        <div><img className="rounded-[10px] h-[400px] w-[1000px] ml-[100px]" src="https://themehealer.com/php-template/crank-php/assets/images/gallery/gallery-1-1.jpg"></img ></div>
+        <div><img  className="rounded-[10px] h-[400px] w-[1000px]  ml-[100px]" src="https://themehealer.com/php-template/crank-php/assets/images/gallery/gallery-1-1.jpg"></img></div>
+        <div><img  className="rounded-[10px] h-[400px] w-[1000px]  ml-[100px]" src="https://themehealer.com/php-template/crank-php/assets/images/gallery/gallery-1-1.jpg"></img></div>
+        
+      </div>
+      
+  </div>
+  </div>
+    
+  
+   
+    
+  </div>
+  
+    <div className="bg-white h-[500px] md:h-[900px] w-[500px] md:w-full rounded-[40px] shadow-2xl flex flex-col mt-[200px] items-center">
+      <div className="text-red-600 text-[15px] md:text-[25px] font-bold">Ask question</div>
+      <div className="text-black font-bold text-[20px] md:text-[40px]">Got Questions? We've Got Answers</div>
+<div className="bg-white border-2  h-[75px] w-[500px] md:w-[1000px] mx-[300px] rounded-[20px] mt-5 mb-10 dark:bg-white dark:text-black"><div className="text-[15px] md:text-[25px] font-bold p-4 ml-[30px]">01.What type of businesses do you support?</div></div>
+<div className="bg-white border-2  h-[75px] w-[500px] md:w-[1000px] mx-[300px] rounded-[20px] mt-5 mb-10 dark:bg-white dark:text-black"><div className="text-[15px] md:text-[25px] font-bold p-4 ml-[30px]">01.What are the signs of brakewear?</div></div>
+<div className="bg-white border-2  h-[75px] w-[500px] md:w-[1000px] mx-[300px] rounded-[20px] mt-5 mb-10 dark:bg-white dark:text-black"><div className="text-[15px] md:text-[25px] font-bold p-4 ml-[30px]">01.Are scheduled service intervals important for my car?</div></div>
+<div className="bg-white border-2  h-[75px] w-[500px] md:w-[1000px] mx-[300px] rounded-[20px] mt-5 mb-10 dark:bg-white dark:text-black"><div className="text-[15px] md:text-[25px] font-bold p-4 ml-[30px]">01.Can regular maintenance prevent major repairs?</div></div>
+    </div>
+
+    
+    
+
+  
+  
+  </div>);
+}
